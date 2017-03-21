0b4cbfabc472dd265712b7dbc689de0e39c8b09a
okletswin <okletswin@gmail.com>
2017-03-21 16:48:44 +0800

    add deb release

--- release.sh
+++ release.sh
@@ -9,11 +9,16 @@ releasedir=$base/$(lsb_release -si)/WORKDIR
 rm -fr $releasedir
 mkdir -p $releasedir
 
-vers=0.1.0
+vers=$(git describe)
 
 mkdir $releasedir/$program-$vers
+
+docker run --rm -it -v `pwd`:/go/src/github.com/barnettzqg/midonet-cni \
+    -w /go/src/github.com/barnettzqg/midonet-cni golang:1.7.3 go build -o $releasedir/$program-$vers/opt/cni/bin/midonet-cni midonet.go
+upx --brute $releasedir/$program-$vers/opt/cni/bin/midonet-cni
+wget lang.goodrain.me/public/loopback -O $releasedir/$program-$vers/opt/cni/bin/loopback
+chmod +x $releasedir/$program-$vers/opt/cni/bin/loopback
 cp -a debian $releasedir/$program-$vers/debian
-cp -a opt $releasedir/$program-$vers/opt
 dvers="${vers}-0"
 
 cd $releasedir/$program-$vers
